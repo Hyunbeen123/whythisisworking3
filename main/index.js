1280a085d583a827071fbb16481f548c4d4e5992
hyunbeen <hyunbinjang0716@gmail.com>
2023-07-16 18:48:46 +0900

    before success

--- main/index.js
+++ main/index.js
@@ -1,3 +1,5 @@
+import { doc, getDoc } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js";
+
 let map;
 
 const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -10,15 +12,22 @@ async function initMap() {
     center: { lat: 35.2122, lng: 128.582 },
     zoom: 17,
   });
+  
   // This event listener calls addMarker() when the map is clicked.
   google.maps.event.addListener(map, "click", (event) => {
     addMarker(event.latLng, map);
-    var popup = window.open("http://www.naver.com", "infopopup", "wodth=500px, height=700px, scrollbars=yes'")
+    var latitude = event.latLng.lat();
+    var longitude = event.latLng.lng();
+    console.log( latitude + ', ' + longitude );
+    var Bcoordinate = { lat: latitude, lng: longitude };
+    let openwin = window.open("http://localhost:65529/info/popup.html", "infopopup", "width=500, height=500, left=100, top=100");
+    // latitude = openwin.document.getElementById("lat_val").value
+    // longitude = openwin.document.getElementById("lng_val").value
+    openwin.onload = function () {
+    openwin.localStorage.setItem('coordinate', JSON.stringify(Bcoordinate));
+    }
   });
-  // Add a marker at the center of the map.
-  addMarker(bangalore, map);
 }
-
 // Adds a marker to the map.
 function addMarker(location, map) {
   // Add the marker at the clicked location, and add the next-available label
@@ -28,8 +37,33 @@ function addMarker(location, map) {
     label: labels[labelIndex++ % labels.length],
     map: map,
   });
-
-  
 }
 
+
+function placeMarkerAndPanTo(latLng, map) {
+  new google.maps.Marker({
+    position: latLng,
+    map: map,
+  });
+  map.panTo(latLng);
+}
+
+
+const docRef = doc(db, "pinName", "");
+const docSnap = await getDoc(docRef);
+
+if (docSnap.exists()) {
+  console.log("Document data:", docSnap.data());
+} else {
+  // docSnap.data() will be undefined in this case
+  console.log("No such document!");
+}
+
+
+
+
+
+
 initMap();
+
+//https://firebase.google.com/docs/firestore/query-data/get-data?hl=ko&authuser=0
